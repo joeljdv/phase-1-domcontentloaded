dd7b2c31b7a432cd10d4a772212870dc16e7f420
joeljdv <joeldiazjdv@gmail.com>
2021-03-23 13:24:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const newId = document.getElementById('text')
+document.addEventListener("DOMContentLoaded",function(){
+    newId.innerHTML="This is really cool!"
+    console.log("The DOM has loaded")
+})
+
+
